42052d007c6e8371eb8050a8b59928e709ecda11
Alexandr Rodionov <rodya3003@yandex.ru>
2024-08-26 22:44:16 +0300

    feature/do index.js & change README.md

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# Проектная работа Mesto
+# Основы JavaScript, базовые типы, работа с DOM (5 спринт)
+## Проектная работа "Mesto на JavaScript"
 
+### Структура:
+- main/ — корневой каталог
+- main/blocks/ — каталог стилей блоков и элементов
+- main/images/ - каталог изображений
+- main/pages/ - каталог импорта стилей
+- main/scripts/ — каталог скриптов
+- main/vendor/ — каталог шрифтов и сброса браузерных стилей
+
+### Файлы:
+- main/index.html — HTML-файл страницы
+- main/scripts/cards.js — массив исходных карточек
+- main/scripts/index.js - логика загрузки и удаления карточки
+- main/vendor/fonts.css - шрифты
+- main/vendor/normalize.css - нормализатор стилей
+
+____
+**Ссылка на проект _[Mesto на JavaScript](https://github.com/alexandr-rodionov/mesto-project-ff.git)_**
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,37 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
 
-// @todo: DOM узлы
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: Функция создания карточки
+// DOM узлы
 
-// @todo: Функция удаления карточки
+const content = document.querySelector('.content');
+const placesList = content.querySelector('.places__list');
 
-// @todo: Вывести карточки на страницу
+// Функция создания карточки
+
+function createCard(srcValue, titleValue, callback) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+
+  cardImage.src = srcValue;
+  cardImage.alt = titleValue;
+  cardElement.querySelector('.card__title').textContent = titleValue;
+
+  cardElement.querySelector('.card__delete-button').addEventListener('click', e => {
+    callback(e.target.parentElement);
+  });
+
+  placesList.append(cardElement);
+}
+
+// Функция удаления карточки
+
+function removeCard(target) {
+  target.remove();
+}
+
+// Вывести карточки на страницу
+
+initialCards.forEach(item => {
+  createCard(item.link, item.name, removeCard);
+});
